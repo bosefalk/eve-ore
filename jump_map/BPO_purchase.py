b3553aa71858be9fb5920e46415b8e5e7ddd9dfe
bose <falk@dkms.de>
2019-02-06 13:20:58 +0100

    Calculate raw materials costs and prices using yesterdays average Amarr price

--- jump_map/BPO_purchase.py
+++ jump_map/BPO_purchase.py
@@ -1,4 +1,6 @@
 import sqlite3
+from jump_map.prices import amarr_price_avg
+import math
 
 conn = sqlite3.connect("jump_map/sqlite-latest.sqlite")
 cur = conn.cursor()
@@ -22,14 +24,22 @@ BPCs = dict(BPCs)
 
 class BPOcost:
 
-    def __init__(self, typeID):
+    def __init__(self, typeID, mat_research = 0.93):
         self.typeID = typeID
+        self.mat_research = mat_research
 
     def add_originBPO(self):
         conn = sqlite3.connect("jump_map/sqlite-latest.sqlite")
         cur = conn.cursor()
-        cur.execute("SELECT productTypeID FROM industryActivityProducts WHERE activityID = 8 AND typeID = " + str(self.typeID))
+        cur.execute("SELECT typeID FROM industryActivityProducts WHERE activityID = 8 AND productTypeID = " + str(self.typeID))
         self.originTypeID = cur.fetchall()[0][0]
+
+    def add_outputItem(self):
+        conn = sqlite3.connect("jump_map/sqlite-latest.sqlite")
+        cur = conn.cursor()
+        cur.execute("SELECT productTypeID FROM industryActivityProducts WHERE activityID = 1 AND typeID = " + str(self.typeID))
+        self.outputItem = cur.fetchall()[0][0]
+
 
     def add_name(self):
         conn = sqlite3.connect("jump_map/sqlite-latest.sqlite")
@@ -44,7 +54,7 @@ class BPOcost:
         self.t2mats = dict(cur.fetchall())
 
         cur.execute("SELECT productTypeID FROM industryActivityProducts WHERE activityID = 1 AND typeID = " + str(self.originTypeID))
-        self.t1ProductID = self.name = cur.fetchall()[0][0]
+        self.t1ProductID = cur.fetchall()[0][0]
 
         del self.t2mats[self.t1ProductID]
 
@@ -54,9 +64,62 @@ class BPOcost:
         cur.execute("SELECT materialTypeID, quantity FROM industryActivityMaterials WHERE activityID = 1 AND typeID = " + str(self.originTypeID))
         self.t1mats = dict(cur.fetchall())
 
+    def cost_t1mats(self, mat_research = 0.93):
+
+        # Look up yesterdays average prices in Amarr for the t1 product, adjusted for the assumed materials
+        # research modifier on the t1 BPO (default 7%)
+        t1cost = list()
+
+        for key, value in self.t1mats.items():
+            amarr_price = amarr_price_avg(key) * (value * 0.93)
+            t1cost.append(amarr_price)
+
+        self.t1cost = round(sum(t1cost))
+
+    def cost_t1mats(self):
+
+        # Look up yesterdays average prices in Amarr for the t1 product, adjusted for the assumed materials
+        # research modifier on the t1 BPO (default 7%)
+        t1cost = list()
+
+        for key, value in self.t1mats.items():
+            amarr_price = amarr_price_avg(key) * (value * self.mat_research)
+            t1cost.append(amarr_price)
+
+        self.t1cost = round(sum(t1cost))
+
+    def cost_t2mats(self):
+
+        # Look up yesterdays average prices in Amarr for the t1 product, adjusted for the assumed materials
+        # research modifier on the t1 BPO (default 7%)
+        t2cost = list()
+
+        for key, value in self.t2mats.items():
+            amarr_price = amarr_price_avg(key) * math.ceil((value * self.mat_research))
+            t2cost.append(amarr_price)
+
+        self.t2cost = round(sum(t2cost))
+
+    def total_cost(self):
+
+        self.cost = self.t1cost + self.t2cost
+
+    def add_price(self):
+
+        amarr_sale_price = amarr_price_avg(self.outputItem)
+        self.price = round(amarr_sale_price)
 
 a = BPOcost(typeID=784)
 a.add_name()
 a.add_originBPO()
 a.add_t2mats()
 a.add_t1mats()
+a.cost_t1mats()
+a.cost_t2mats()
+a.total_cost()
+a.add_outputItem()
+a.add_price()
+
+print(a.cost)
+print(a.price)
+print(a.price - a.cost)
--- jump_map/prices.py
+++ jump_map/prices.py
@@ -2,44 +2,80 @@
 
 import urllib.request
 import json
+import datetime
 
-price_json = urllib.request.urlopen("https://esi.evetech.net/latest/markets/prices/?datasource=tranquility").read()
+ID = 35
 
-price_dict = json.loads(price_json)
+def amarr_price_now(ID):
+    # Domain is region ID 10000043
+    price_json = urllib.request.urlopen("https://esi.evetech.net/latest/markets/10000043/orders/?datasource=tranquility&order_type=sell&page=1&type_id=" + str(ID)).read()
+    price_dict = json.loads(price_json)
 
-# Type IDs are from the sqlite-lastest.sqlite database, downloaded from https://www.fuzzwork.co.uk/dump/
+    # Amarr has solarsystem ID 30002187
 
-mineral_ids = {"Tritanium": 34,
-               "Pyerite": 35}
+    price_dict = [dict for dict in price_dict if dict['system_id'] == 30002187]
+    prices = [dict['price'] for dict in price_dict]
+    min_price = min(prices)
+    return(min_price)
 
+def amarr_price_avg(ID):
 
-l = list()
-types = [32774, 32780]
+    # Domain is region ID 10000043
+    price_json = urllib.request.urlopen("https://esi.evetech.net/latest/markets/10000043/history/?datasource=tranquility&type_id=" + str(ID)).read()
+    price_dict = json.loads(price_json)
 
-l = [id_dict for id_dict in price_dict if id_dict['type_id'] in mineral_ids.values()]
+    yesterday = datetime.datetime.today() - datetime.timedelta(days = 1)
+    yesterday_str = yesterday.strftime("%Y-%m-%d")
 
-volume = 10000
-time_cost = 5,
-jump_time = 45)
+    yesterday_avg = [dict['average'] for dict in price_dict if dict['date'] == yesterday_str]
 
-price_trit = urllib.request.urlopen("https://esi.evetech.net/latest/markets/10000065/orders/?datasource=tranquility&order_type=buy&page=1&type_id=35").read()
-price_trit = json.loads(price_trit)
+    while len(yesterday_avg) == 0:
+        yesterday = yesterday - datetime.timedelta(days = 1)
+        yesterday_str = yesterday.strftime("%Y-%m-%d")
+        yesterday_avg = [dict['average'] for dict in price_dict if dict['date'] == yesterday_str]
 
-# Filter sales orders which can accept this volume of minerals
-price_trit = [l for l in price_trit if (volume <= l['volume_remain']) and (volume >= l['min_volume'])]
+    return(yesterday_avg[0])
 
-
-calc_penalized_value(time_cost, jump_time, volume):
-    needed_jumps = jumps - range
-    if self.needed_jumps < 0:
-        self.needed_jumps = 0
-
-    self.value = volume * self.price
-
-    self.jump_cost = self.needed_jumps * ((time_cost * 1000000 / 3600) * jump_time)
-
-    self.penalized_value = self.value - self.jump_cost
-
-
-
-i['type_id'] is
+#
+#
+#
+# price_json = urllib.request.urlopen("https://esi.evetech.net/latest/markets/prices/?datasource=tranquility").read()
+#
+#
+#
+# # Type IDs are from the sqlite-lastest.sqlite database, downloaded from https://www.fuzzwork.co.uk/dump/
+#
+# mineral_ids = {"Tritanium": 34,
+#                "Pyerite": 35}
+#
+#
+# l = list()
+# types = [32774, 32780]
+#
+# l = [id_dict for id_dict in price_dict if id_dict['type_id'] in mineral_ids.values()]
+#
+# volume = 10000
+# time_cost = 5,
+# jump_time = 45)
+#
+# price_trit = urllib.request.urlopen("https://esi.evetech.net/latest/markets/10000065/orders/?datasource=tranquility&order_type=buy&page=1&type_id=35").read()
+# price_trit = json.loads(price_trit)
+#
+# # Filter sales orders which can accept this volume of minerals
+# price_trit = [l for l in price_trit if (volume <= l['volume_remain']) and (volume >= l['min_volume'])]
+#
+#
+# calc_penalized_value(time_cost, jump_time, volume):
+#     needed_jumps = jumps - range
+#     if self.needed_jumps < 0:
+#         self.needed_jumps = 0
+#
+#     self.value = volume * self.price
+#
+#     self.jump_cost = self.needed_jumps * ((time_cost * 1000000 / 3600) * jump_time)
+#
+#     self.penalized_value = self.value - self.jump_cost
+#
+#
+#
+# i['type_id'] is
